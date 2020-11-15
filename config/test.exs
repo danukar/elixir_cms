9017421c30a0b29cc158b6b1802e9aada78abfa2
Danuka Rajapaksha <44248804+danukar@users.noreply.github.com>
2020-11-15 23:45:42 +0800

    Add files via upload

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,5 @@ config :logger, level: :warn
 # Configure your database
 config :cms, Cms.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "rails",
-  password: "rails",
-  database: "cms_test",
-  hostname: "localhost",
+  url: System.get_env("DATABASE_URL"),
   pool: Ecto.Adapters.SQL.Sandbox
